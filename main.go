ae11400088ccddef8579af0e839193c52650551f
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2021-12-23 10:56:47 +0900

    test

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 	)
 
-	message := linebot.NewTextMessage("hello, world")
+	message := linebot.NewTextMessage("hello, yuki")
 	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
 		log.Fatal(err)
 	}
