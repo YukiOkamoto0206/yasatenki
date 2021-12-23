875718254ba3b112c3b72f3c76accdc5b13a4f1a
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2021-12-23 20:39:32 +0900

    refs: env削除

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"github.com/YukiOkamoto0206/yasatenki/weather"
-	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		// .env読めなかった場合の処理
-		log.Fatal(err)
-	}
+	//err := godotenv.Load(".env")
+	//if err != nil {
+	//	// .env読めなかった場合の処理
+	//	log.Fatal(err)
+	//}
 	bot, _ := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
