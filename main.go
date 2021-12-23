f7d52952a36b46b1330133b3d9914e3c0eb9317d
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2021-12-23 10:52:13 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		// .env読めなかった場合の処理
+	}
+	bot, _ := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+
+	message := linebot.NewTextMessage("hello, world")
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+}
